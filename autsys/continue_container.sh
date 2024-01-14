85be2e97505051e9981d82c6943318756947384f
Cem Küçükgenç <cem.kucukgenc@tum.de>
2024-01-14 17:15:58 +0100

    autsys updated, innok deleted

--- autsys/continue_container.sh
+++ autsys/continue_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 container_name="autsys_container" # Change this to your container name
-catkin_ws_path="/autsys_ws/autsys-2023-geometric-controller-cemkucukgenc/catkin_ws" # Change this to your catkin_ws path
+catkin_ws_path="/autsys_ws/tum_autsys_project/catkin_ws" # Change this to your catkin_ws path
 
 # ANSI escape codes for colors
 RED='\033[0;31m'
@@ -9,20 +9,28 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RESET='\033[0m'
 
-sudo docker exec -it $container_name bash -c \
-  "source /opt/ros/noetic/setup.bash && \
-   if [ \$? -eq 0 ]; then
-     echo -e '${GREEN}ROS distro sourced as${RESET}' \$ROS_DISTRO 
-   else
-     echo -e '${RED}Failed to source ROS distro${RESET}' 
-   fi && \
-   source $catkin_ws_path/devel/setup.bash && \
-   if [ \$? -eq 0 ]; then
-     echo -e '${GREEN}ROS workspace sourced as${RESET}' $catkin_ws_path
-   else
-     echo -e '${RED}Failed to source ROS workspace${RESET}'
-   fi && \
-   bash"
+sudo docker exec -it $container_name bash -c "
+  if [ -f '/opt/ros/noetic/setup.bash' ]; then
+      source /opt/ros/noetic/setup.bash && \
+      if [ \$? -eq 0 ]; then
+          echo -e '${GREEN}ROS distro sourced as${RESET}' \$ROS_DISTRO 
+      else
+          echo -e '${RED}Failed to source ROS distro${RESET}' 
+      fi
+  else
+      echo -e '${YELLOW}/opt/ros/noetic/setup.bash does not exist yet. Skipping sourcing.${RESET}'
+  fi && \
+  if [ -f '$catkin_ws_path/devel/setup.bash' ]; then
+      source $catkin_ws_path/devel/setup.bash && \
+      if [ \$? -eq 0 ]; then
+          echo -e '${GREEN}ROS workspace sourced as${RESET}' $catkin_ws_path
+      else
+          echo -e '${RED}Failed to source ROS workspace${RESET}' $catkin_ws_path
+      fi
+  else
+      echo -e '${YELLOW}$catkin_ws_path/devel/setup.bash does not exist yet. Skipping sourcing.${RESET}'
+  fi && \
+  bash"
 
 exit 0
 
--- autsys/rosource.sh
+++ autsys/rosource.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-catkin_ws_path="/autsys_ws/autsys-2023-geometric-controller-cemkucukgenc/catkin_ws" # Change this to your catkin_ws path
+catkin_ws_path="/autsys_ws/tum_autsys_project/catkin_ws" # Change this to your catkin_ws path
 
 source $catkin_ws_path/devel/setup.bash
 
@@ -10,10 +10,15 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RESET='\033[0m'
 
-if [ -n "$ROS_ROOT" ]; then
-    echo -e "${GREEN}ROS workspace sourced as${RESET}" $catkin_ws_path
+if [ -f "$catkin_ws_path/devel/setup.bash" ]; then
+    source $catkin_ws_path/devel/setup.bash
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}ROS workspace sourced as${RESET}" $catkin_ws_path
+    else
+        echo -e "${RED}Failed to source ROS workspace${RESET}" $catkin_ws_path
+    fi
 else
-    echo -e "${RED}Failed to source ROS workspace${RESET}"
+    echo -e "${YELLOW}$catkin_ws_path/devel/setup.bash does not exist yet. Skipping sourcing.${RESET}"
 fi
 
 exit 0
